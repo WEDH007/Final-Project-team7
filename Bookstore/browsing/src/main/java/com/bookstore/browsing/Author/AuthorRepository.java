5783344285ad122853c5737663d4fab9b28ab477
William E Duran <95000547+WEDH007@users.noreply.github.com>
2023-07-16 22:28:41 -0500

    Delete Bookstore directory

--- Bookstore/browsing/src/main/java/com/bookstore/browsing/Author/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.browsing.Author;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
--- Bookstore/browsing/src/main/java/com/bookstore/browsing/Publisher/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.browsing.Publisher;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
-}
